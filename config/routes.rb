a83ff86873d1b70d8def80857bb37e4d6e5f4f08
mperea9 <mperea97@gmail.com>
2020-05-03 18:36:10 -0600

    test6

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,18 @@ VaxGame::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-  match '/home', to: 'static_pages#home'
-  match '/tour', to: 'static_pages#tour'
-  match '/help', to: 'static_pages#faq'
-  match '/faq', to: 'static_pages#faq'
-  match '/about', to: 'static_pages#faq'
-  match '/contact', to: 'static_pages#faq'
-  match '/herdImmunity', to: 'static_pages#herdImmunity'
-  match '/herdimmunity', to: 'static_pages#herdImmunity'
-  match '/scenario', to: 'static_pages#scenarioSelect'
-  match '/scenarioGame', to: 'static_pages#scenarioGame'
-  match '/scores', to: 'scores#new'
+  match '/home', to: 'static_pages#home', :via => [:get, :post]
+  match '/tour', to: 'static_pages#tour', :via => [:get, :post]
+  match '/help', to: 'static_pages#faq', :via => [:get, :post]
+  match '/faq', to: 'static_pages#faq', :via => [:get, :post]
+  match '/about', to: 'static_pages#faq', :via => [:get, :post]
+  match '/contact', to: 'static_pages#faq', :via => [:get, :post]
+  match '/herdImmunity', to: 'static_pages#herdImmunity', :via => [:get, :post]
+  match '/herdimmunity', to: 'static_pages#herdImmunity', :via => [:get, :post]
+  match '/scenario', to: 'static_pages#scenarioSelect', :via => [:get, :post]
+  match '/scenarioGame', to: 'static_pages#scenarioGame', :via => [:get, :post]
+  match '/scores', to: 'scores#new', :via => [:get, :post]
 
-  match '/game', to: 'static_pages#game'
+  match '/game', to: 'static_pages#game', :via => [:get, :post]
 
 end
